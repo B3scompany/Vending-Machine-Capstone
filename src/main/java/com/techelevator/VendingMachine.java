de8141a5a2607f7d277c1b597f833c53f3d1e369
Spencer Walker <walkersl0209@gmail.com>
2022-02-10 17:20:58 -0500

    not complete

---
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,36 @@
+package com.techelevator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class VendingMachine {
+
+
+        FileReader fileReader = new FileReader("vendingmachine.csv");
+        List<String> snackList = new ArrayList<String>();
+        private List<>
+        public VendingMachine() {
+
+            while ()
+
+    }
+
+//    private static File getDestinationFileFromUser(Scanner i){
+//        System.out.println("What is the destination file? ");
+//        String path = i.nextLine();
+//
+//        File destinationFile = new File(path);
+//        try {
+//            destinationFile.createNewFile();
+//        }
+//        catch (IOException e) {
+//            System.out.println("Cant access file");
+//        }
+//        return destinationFile;
+//    }
+}
